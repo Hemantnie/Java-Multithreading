b91ed56732a79fd786424c8988ab1e851d589cf2
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-13 00:23:38 +0530

    Replacing extending thread with implementing runnable class

--- src/com/practice/RealHelloWorldSingleThreaded.java
+++ src/com/practice/RealHelloWorldSingleThreaded.java
@@ -2,10 +2,9 @@ package com.practice;
 
 public class RealHelloWorldSingleThreaded {
 	
-	public static class Greeter extends Thread{
+	public static class Greeter implements Runnable{
 		private String country;
 		public Greeter(String country) {
-			super(country+"thread");
 			this.country = country;
 		}
 		public void run() {
@@ -17,7 +16,8 @@ public class RealHelloWorldSingleThreaded {
 		String countries[] = {"France","India","China","USA","Germany"};
 		
 		for(String country:countries) {
-			new Greeter(country).start();
+			Greeter greeter = new Greeter(country);
+			new Thread(greeter,country+"thread").start();
 		}
 
 	}
